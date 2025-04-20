d4d974a872c42610d30deec06d63fdc9543f7be6
Tenshi Hinanawi <203447253+TenshiSWR@users.noreply.github.com>
2025-04-20 22:38:40 +0100

    Refactor, added move+create protection detection

--- tasks/rmtr.py
+++ tasks/rmtr.py
@@ -23,10 +23,11 @@ instructions, uncontroversial_requests, undiscussed_moves, contested_requests, a
                                                                                                      split_text_by_line[section_indexes[2]:section_indexes[3]], \
                                                                                                      split_text_by_line[section_indexes[3]:]
 
-actions = {"technical": 0, "RMUM": 0, "contested": 0}
+actions = {"technical": 0, "RMUM": 0, "contested": 0, "administrator": 0, "moved": 0}
 
 
-def process_non_contested_requests(section_queue):
+def process_non_contested_requests(section_queue, section_group):
+    global administrator_moves
     requests = []
     number_to_update_by = 0
     for i, line in enumerate(section_queue):
@@ -44,7 +45,7 @@ def process_non_contested_requests(section_queue):
             # A moved page will either have a redirect (normal move) or no text on it (suppressredirect).
             # Prevents accidentally picking up previous moves and removing the request when it hasn't been done.
             if (base_page.moved_target() == target_page) and (base_page.isRedirectPage() or not len(base_page.text)):
-                print(base_page.title()+" has been moved, removing non-contested request.")
+                print(base_page.title()+" has been moved, removing {} request.".format(section_group))
                 try:
                     number_to_update_by = requests[i+1][0]-requests[i][0]
                     #print((requests[i][0], number_to_update_by, requests[i+1][0]), section_queue[requests[i][0]:requests[i+1][0]])
@@ -54,11 +55,23 @@ def process_non_contested_requests(section_queue):
                     del section_queue[requests[i][0]:len(section_queue)-1]
                 finally:
                     requests = [[x[0]-number_to_update_by, x[1]] for x in requests]
-                    if section_queue[0] == "==== Uncontroversial technical requests ====":
-                        actions["technical"] += 1
-                    else:
-                        actions["RMUM"] += 1
-        except (pywikibot.exceptions.NoMoveTargetError, ):
+                    actions[{"Uncontroversial technical requests":"technical", "Requests to revert undiscussed moves":"RMUM", "Administrator needed":"moved"}[section_group]] += 1
+            elif section_group != "Administrator needed":  # Check protections on both pages
+                protections = [base_page.protection(), target_page.protection()]
+                if ({"move":("sysop", "infinity")} or {"create":("sysop", "infinity")} in protections[0].items()) or ({"move":("sysop", "infinity")} or {"create":("sysop", "infinity")} in protections[1].items()):
+                    print("One or both pages mentioned in {} --> {} are either create-protected or move-protected. Moving to Administrator needed section".format(base_page.title(), target_page.title()))
+                    try:  # This could probably be refactored into a single common function at some point
+                        number_to_update_by = requests[i+1][0]-requests[i][0]
+                        administrator_moves += section_queue[requests[i][0]:requests[i+1][0]]
+                        del section_queue[requests[i][0]:requests[i+1][0]]
+                    except (IndexError, ValueError):
+                        administrator_moves += section_queue[requests[i][0]:len(section_queue)-1]
+                        del section_queue[requests[i][0]:len(section_queue)-1]
+                    finally:
+                        requests = [[x[0]-number_to_update_by, x[1]] for x in requests]
+                        administrator_moves.append("::One or both pages mentioned are either create-protected or move-protected. It has been moved from the {} section. ~~~~".format(section_group))
+                        actions["moved"] += 1
+        except pywikibot.exceptions.NoMoveTargetError:
             pass
         except pywikibot.exceptions.InvalidTitleError:
             print("Bad request (invalid title error): "+str(requests[i][1]))
@@ -84,7 +97,7 @@ def process_contested_requests(section_queue):
             initial_request, whole_request, indexes = mwparserfromhell.parse(section_queue[requests[i][0]]), "".join(section_queue[requests[i][0]:len(section_queue)-1]), (requests[i][0], len(section_queue)-1)
         last_reply = MediawikiApi.get_last_reply(None, whole_request)
         if (datetime.datetime.now().replace(tzinfo=None)-datetime.timedelta(hours=72)) > last_reply.replace(tzinfo=None):
-            print("Removing expired contested request:", str(initial_request.filter_templates()[0].get(1).value)+" --> "+str(initial_request.filter_templates()[0].get(2).value))
+            print("Removing expired contested request: {} --> {}".format(initial_request.filter_templates()[0].get(1).value, initial_request.filter_templates()[0].get(2).value))
             notify_requesters(initial_request.filter_templates()[0].get("requester").value, initial_request.filter_templates()[0].get(1).value)
             try:
                 number_to_update_by = requests[i+1][0]-requests[i][0]
@@ -101,7 +114,7 @@ def process_contested_requests(section_queue):
 
 
 def notify_requesters(requester, article):
-    article_talk_page = pywikibot.Page(site, "Talk:{}".format(article))
+    article_talk_page = pywikibot.Page(site, "{}".format(article)).toggleTalkPage()
     for template in mwparserfromhell.parse(article_talk_page.text).filter_templates():
         if template.name.matches("Requested move/dated"):
             print("RM started on talk page of {}, not notifying {}".format(article, requester))
@@ -124,9 +137,10 @@ def reassemble_page():
     return "\n".join(instructions+uncontroversial_requests+undiscussed_moves+contested_requests+administrator_moves)
 
 
-uncontroversial_requests = process_non_contested_requests(uncontroversial_requests)
-undiscussed_moves = process_non_contested_requests(undiscussed_moves)
+uncontroversial_requests = process_non_contested_requests(uncontroversial_requests, "Uncontroversial technical requests")
+undiscussed_moves = process_non_contested_requests(undiscussed_moves, "Requests to revert undiscussed moves")
 contested_requests = process_contested_requests(contested_requests)
-if actions["technical"]+actions["RMUM"]+actions["contested"] > 0:  # Check to see if anything has been done before saving an edit.
+administrator_moves = process_non_contested_requests(administrator_moves, "Administrator needed")
+if sum([action for action in actions.values()]) > 0:  # Check to see if anything has been done before saving an edit.
     rmtr.text = reassemble_page()
-    rmtr.save(summary="Userspace testing: Clerk [[Wikipedia:Requested moves/Technical requests|RM/TR]]. Processed {} technical requests, {} move revert requests and {} contested requests.".format(actions["technical"], actions["RMUM"], actions["contested"]), minor=False)
+    rmtr.save(summary="Userspace testing: Clerk [[Wikipedia:Requested moves/Technical requests|RM/TR]]. Processed {} requests.".format(sum([action for action in actions.values()])), minor=False)
