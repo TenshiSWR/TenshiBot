ad8d1a3fd3928b1301037ec677dec0f003aa6baa
Tenshi Hinanawi <203447253+TenshiSWR@users.noreply.github.com>
2025-05-28 00:17:01 +0100

    Add report

---
+++ staleuserspacedraftsbyindeffedusers.py
@@ -0,0 +1,26 @@
+import pywikibot
+site = pywikibot.Site()
+userspace_drafts = [userspace_draft for userspace_draft in pywikibot.Category(site, "Stale userspace drafts").articles()]
+indeffed_user_drafts = {}
+for userspace_draft in userspace_drafts:
+    user = pywikibot.User(site, userspace_draft.oldest_revision.user)
+    if user.is_blocked():
+        print(userspace_draft.title())
+        try:
+            indeffed_user_drafts[user].append(userspace_draft.title())
+        except KeyError:
+            indeffed_user_drafts[user] = [userspace_draft.title()]
+
+
+# Table format from User:AnomieBOT/Nobots Hall of Shame
+report_page = pywikibot.Page(site, "User:TenshiBot/Stale userspace drafts by blocked users")
+report_page.text = "\n".join(['{{/header}}',
+                              '{| class="wikitable sortable" style="width:100%"',
+                              '!Username!!Number of userspace drafts!!class="unsortable"|Pages\n'])
+for username, pages in indeffed_user_drafts.items():
+    report_page.text += "\n".join(["|-", f"| {username}", f"| {len(pages)}", "|{{hidden begin}}\n"])
+    report_page.text += "\n".join(["* [[{}]]".format(page) for page in pages])+"\n"
+    report_page.text += "{{hidden end}}\n"
+report_page.text += "|}"
+report_page.save(summary="Adding report data", minor=False)
+
