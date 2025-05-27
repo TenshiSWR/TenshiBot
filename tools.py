39c6afd70865e3e39b925ba32d8ebaf56042690a
Tenshi Hinanawi <203447253+TenshiSWR@users.noreply.github.com>
2025-05-27 13:58:11 +0100

    Move newline

--- tools.py
+++ tools.py
@@ -12,8 +12,9 @@ def log_error(error: str, task_number: int):
     import pywikibot
     site = pywikibot.Site()
     error_page = pywikibot.Page(site, "User:TenshiBot/Errors")
-    error_page.text += "{} (Task {}): {}\n".format(datetime.utcnow().strftime("[%Y-%m-%d %H:%M]"), str(task_number), error)
-    error_page.save(summary="Logging error during task {}".format(str(task_number)), minor=False)
+    error_page.text += "\n{} (Task {}): {}".format(datetime.utcnow().strftime("[%Y-%m-%d %H:%M]"), str(task_number), error)
+    error_page.save(summary="Logging error during task {}".format(str(task_number)), minor=False, bot=True)
+
 
 def mediawikitimestamp_to_datetime(mediawikitimestamp: str):
     from datetime import datetime
