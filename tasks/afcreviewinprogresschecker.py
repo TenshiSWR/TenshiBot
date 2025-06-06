5ee7b5a4027b5931c121ea1115ec57e444a0431d
Tenshi Hinanawi <203447253+TenshiSWR@users.noreply.github.com>
2025-06-06 13:34:56 +0100

    Remove 1 quote mark

--- tasks/afcreviewinprogresschecker.py
+++ tasks/afcreviewinprogresschecker.py
@@ -31,7 +31,7 @@ def check_notified(user: str):
     connection = toolforge.toolsdb("s56602__afc_notifications_p")
     with connection.cursor() as cursor:
         print(repr(user))
-        cursor.execute("SELECT time FROM long_reviews WHERE user = '%(username)s';", {"username": user})
+        cursor.execute("SELECT time FROM long_reviews WHERE user = '%(username)s;", {"username": user})
         time = cursor.fetchone()
         connection.close()
         print("Check_notified (query response):", str(time))
