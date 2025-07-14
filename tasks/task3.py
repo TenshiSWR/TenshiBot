74841fc18375090f5119712cae639610bb728bef
Tenshi Hinanawi <tenshihinanawiSWR@gmail.com>
2025-07-14 12:31:53 +0100

    Add redirect

--- tasks/task3.py
+++ tasks/task3.py
@@ -5,7 +5,7 @@ talk_pages = [talk_page for talk_page in pywikibot.Category(site, "Unknown-impor
 for talk_page in talk_pages:
     parsed_text = mwparserfromhell.parse(talk_page.text)
     for template in parsed_text.filter_templates():
-        if template.name.matches("WikiProject Canada"):
+        if template.name.matches("WikiProject Canada") or template.name.matches("WP Canada"):
             template.add("ns-importance", "low")
             print("{}: Parameter added".format(talk_page.title()))
             talk_page.text = parsed_text
