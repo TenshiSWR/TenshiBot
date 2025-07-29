5eadd471ed269d2bde147ea8541d66a8fcd8d9f3
Tenshi Hinanawi <203447253+TenshiSWR@users.noreply.github.com>
2025-07-29 02:34:02 +0100

    New report

---
+++ reports/albumarticleswithouttracklistings.py
@@ -0,0 +1,34 @@
+from pwiki.mquery import MQuery
+from pwiki.wiki import Wiki
+import pywikibot
+import re
+mquery, wiki = MQuery(), Wiki()
+working_list = wiki.what_transcludes_here("Template:Infobox album")
+text = mquery.page_text(wiki, working_list)
+index = 0
+
+while index < len(working_list):
+    working_text = text[working_list[index]]
+    result = re.search("Category: *.*(Albums|albums)", working_text)
+    if not result:  # 1. Has to have "albums" in it
+        working_list.pop(index)
+        continue
+    result = re.search("==* *Track listing", working_text, re.I)
+    if result:
+        working_list.pop(index)
+        continue
+    # 2. Must not have track listing
+    print("{} does not have a track listing".format(working_list[index]))
+    index += 1
+    continue
+
+site = pywikibot.Site()
+page = pywikibot.Page(site, "User:TenshiBot/Album articles without track listings")
+page.text = "Album articles without track listings:"
+if len(working_list) > 0:
+    for article in sorted(working_list):
+        page.text += "\n* [[{}]]".format(article)
+else:
+    page.text += "\nNo results found."
+page.save(summary="Create report", minor=False)
+
