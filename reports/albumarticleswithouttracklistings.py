0c8d37dddeac53d099c25bec9a07fb57cac6e6e3
Tenshi Hinanawi <203447253+TenshiSWR@users.noreply.github.com>
2025-07-29 14:21:53 +0100

    Update regex

--- reports/albumarticleswithouttracklistings.py
+++ reports/albumarticleswithouttracklistings.py
@@ -11,10 +11,12 @@ while index < len(working_list):
     working_text = text[working_list[index]]
     result = re.search("Category: *.*(Albums|albums)", working_text)
     if not result:  # 1. Has to have "albums" in it
+        del text[working_list[index]]
         working_list.pop(index)
         continue
-    result = re.search("==* *Track listing", working_text, re.I)
+    result = re.search("==* *(Track listing|Track list|Tracklist|Disc 1|Disc 2|CD track listing|DVD track listing|Set listing|Tracks|Tracklisting)", working_text, re.I)
     if result:
+        del text[working_list[index]]
         working_list.pop(index)
         continue
     # 2. Must not have track listing
@@ -30,5 +32,5 @@ if len(working_list) > 0:
         page.text += "\n* [[{}]]".format(article)
 else:
     page.text += "\nNo results found."
-page.save(summary="Create report", minor=False)
+page.save(summary="Updating report", minor=False)
 
