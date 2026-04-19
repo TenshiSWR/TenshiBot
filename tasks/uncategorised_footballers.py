159b0e8ec56b6ad1f6563cfc1b94f06d63a9215f
Tenshi Hinanawi <203447253+TenshiSWR@users.noreply.github.com>
2026-04-19 14:29:34 +0100

    Another test

--- tasks/uncategorised_footballers.py
+++ tasks/uncategorised_footballers.py
@@ -4,7 +4,7 @@ from tools.misc import log_error
 from tools.summaries import EDIT_FAIL_SUMMARY, TASK11_SUMMARY
 
 site = pywikibot.Site()
-player_cats = ["Category:Association football players by club", "Category:Association football players by competition", "Category:Association football players by country", "Category:Association football players by national team", "Category:Association football players by nationality", "Category:Association football players by populated place", "Category:Expatriate association football players", "Category:Women's association football players"]
+player_cats = ["Category:Association football players by club", "Category:Association football players by competition", "Category:Association football players by country", "Category:Association football players by national team", "Category:Association football players by nationality", "Category:Association football players by populated place", "Category:Expatriate association football players", "Category:Men's association football players not categorized by position", "Category:Women's association football players", "Category:Women's association football players not categorized by position"]
 position_cats = ["Category:Association football defenders", "Category:Association football forwards", "Category:Association football goalkeepers", "Category:Association football midfielders", "Category:Association football wingers", "Category:Men's association football central defenders", "Category:Men's association football full-backs", "Category:Men's association football inside forwards", "Category:Men's association football outside forwards", "Category:Men's association football sweepers", "Category:Men's association football wing halves", "Category:Women's association football defenders", "Category:Women's association football forwards", "Category:Women's association football goalkeepers", "Category:Women's association football midfielders"]
 excluded_cats = ["Category:Association football player non-biographical articles", "Category:Association football trophies and awards"]
 
@@ -48,13 +48,15 @@ for page in pages:
             elif search(r"Category:.*(?<!o)[Mm]en's", page.text):
                 _ = "Men's"
             try:
-                #print("Would edit {} (removal)".format(page.title()))
+                print("Would edit {} (removal)".format(page.title()))
                 i += 1
-                page.save(summary=TASK11_SUMMARY.format("Removal", _), minor=False)
+                #page.save(summary=TASK11_SUMMARY.format("Removal", _), minor=False)
             except (pywikibot.exceptions.EditConflictError, pywikibot.exceptions.LockedPageError, pywikibot.exceptions.OtherPageSaveError):
                 log_error(EDIT_FAIL_SUMMARY.format(page.title()), 11, soft=True)
             break
     else:
+        if not search(r"(?:is|was) an? .*?(?:footballer|(?:football|soccer) player).*?\." page.text):
+            continue
         if search(r"Category:.*[Ww]omen's", page.text):
             _ = "Women's"
         elif search(r"Category:.*(?<!o)[Mm]en's", page.text):
@@ -63,9 +65,9 @@ for page in pages:
             continue
         page.text += "\n[[Category:"+_+" association football players not categorized by position]]"
         try:
-            #print("Would edit {} (addition)".format(page.title()))
+            print("Would edit {} (addition)".format(page.title()))
             i += 1
-            page.save(summary=TASK11_SUMMARY.format("Addition", _), minor=False)
+            #page.save(summary=TASK11_SUMMARY.format("Addition", _), minor=False)
         except (pywikibot.exceptions.EditConflictError, pywikibot.exceptions.LockedPageError, pywikibot.exceptions.OtherPageSaveError):
             log_error(EDIT_FAIL_SUMMARY.format(page.title()), 11, soft=True)
     if i >= 100:
