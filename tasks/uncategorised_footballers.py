3c1061294df8d09e6c18a1fbe3798ffc00b9735f
Tenshi Hinanawi <203447253+TenshiSWR@users.noreply.github.com>
2026-04-27 11:27:16 +0100

    Various fixes, another dummy run

--- tasks/uncategorised_footballers.py
+++ tasks/uncategorised_footballers.py
@@ -1,11 +1,11 @@
 import pywikibot
-from regex import search, sub
+import regex
 from tools.misc import log_error
 from tools.summaries import EDIT_FAIL_SUMMARY, TASK11_SUMMARY
 
 site = pywikibot.Site()
 player_cats = ["Category:Association football players by club", "Category:Association football players by competition", "Category:Association football players by country", "Category:Association football players by national team", "Category:Association football players by nationality", "Category:Association football players by populated place", "Category:Expatriate association football players", "Category:Men's association football players not categorized by position", "Category:Women's association football players", "Category:Women's association football players not categorized by position"]
-position_cats = ["Category:Men's association football central defenders", "Category:Men's association football defenders", "Category:Men's association football forwards", "Category:Men's association football full-backs", "Category:Men's association football goalkeepers", "Category:Men's association football inside forwards", "Category:Men's association football midfielders", "Category:Men's association football outside forwards", "Category:Men's association football sweepers", "Category:Men's association football wing halves", "Category:Men's association football wingers", "Category:Women's association football central defenders", "Category:Women's association football defenders", "Category:Women's association football forwards", "Category:Women's association football full-backs", "Category:Women's association football goalkeepers", "Category:Women's association football midfielders", "Category:Women's association football outside forwards", "Category:Women's association football sweepers", "Category:Women's association football wingers"]
+position_cats = ["Category:Men's association football central defenders", "Category:Men's association football defenders", "Category:Men's association football forwards", "Category:Men's association football full-backs", "Category:Men's association football goalkeepers", "Category:Men's association football inside forwards", "Category:Men's association football midfielders", "Category:Men's association football outside forwards", "Category:Men's association football sweepers", "Category:Men's association football utility players", "Category:Men's association football wing halves", "Category:Men's association football wingers", "Category:Women's association football central defenders", "Category:Women's association football defenders", "Category:Women's association football forwards", "Category:Women's association football full-backs", "Category:Women's association football goalkeepers", "Category:Women's association football midfielders", "Category:Women's association football outside forwards", "Category:Women's association football sweepers", "Category:Women's association football wingers", "Category:Women's association football utility players"]
 excluded_cats = ["Category:Association football player non-biographical articles", "Category:Association football trophies and awards"]
 
 
@@ -39,38 +39,45 @@ for page in pages:
         continue
     page = pywikibot.Page(site, page)
     for position_cat in position_cats:
-        if position_cat in page.text and not search(r"Category:(?:[Ww]o)?[Mm]en's association football players not categorized by position", page.text):
+        if position_cat in page.text and not regex.search(r"Category:(?:[Ww]o)?[Mm]en's association football players not categorized by position", page.text):
             break
-        elif position_cat in page.text and search(r"Category:(?:[Ww]o)?[Mm]en's association football players not categorized by position", page.text):
-            page.text = sub(r"\n\[\[Category:(?:[Ww]o)?[Mm]en's association football players not categorized by position\]\]", "", page.text)
-            if search(r"Category:.*[Ww]omen's", page.text):
+        elif position_cat in page.text and regex.search(r"Category:(?:[Ww]o)?[Mm]en's association football players not categorized by position", page.text):
+            page.text = regex.sub(r"\n\[\[Category:(?:[Ww]o)?[Mm]en's association football players not categorized by position\]\]", "", page.text)
+            if regex.search(r"Category:.*[Ww]omen's", page.text):
                 _ = "Women's"
-            elif search(r"Category:.*(?<!o)[Mm]en's", page.text):
+            elif regex.search(r"Category:.*(?<!o)[Mm]en's", page.text):
                 _ = "Men's"
             try:
-                #print("Would edit {} (removal)".format(page.title()))
                 i += 1
-                page.save(summary=TASK11_SUMMARY.format("Removal", _), minor=False)
+                if i >= 100:
+                    print("Would edit {} (removal)".format(page.title()))
+                #page.save(summary=TASK11_SUMMARY.format("Removal", _), minor=False)
             except (pywikibot.exceptions.EditConflictError, pywikibot.exceptions.LockedPageError, pywikibot.exceptions.OtherPageSaveError):
                 log_error(EDIT_FAIL_SUMMARY.format(page.title()), 11, soft=True)
             break
     else:
-        if not search(r"(?:is|was) an? .*?(?:footballer|(?:football|soccer) player).*?\.", page.text):
+        if not regex.search(r"(?:is|was) an? .*?(?:footballer|(?:football|soccer) player).*?\.", page.text):
             continue
-        if search(r"Category:.*[Ww]omen's", page.text):
+        elif regex.search(r"\[\[Category:(?:[Ww]o)?[Mm]en's association football players not categorized by position\]\]", page.text):
+            continue
+        if regex.search(r"Category:.*[Ww]omen's", page.text):
             _ = "Women's"
-        elif search(r"Category:.*(?<!o)[Mm]en's", page.text):
+        elif regex.search(r"Category:.*(?<!o)[Mm]en's", page.text):
             _ = "Men's"
         else:
             continue
-        page.text += "\n[[Category:"+_+" association football players not categorized by position]]"
+        if regex.search(r"{{.*?-stub}}", page.text):
+            page.text = regex.sub(r"(\[\[Category:[^]]*?\]\])([^[]*?)(\{\{.*?-stub\}\})", r"\1\n[[Category:"+_+" association football players not categorized by position]]\2\3", flags=regex.DOTALL)
+        else:
+            page.text += "\n[[Category:"+_+" association football players not categorized by position]]"
         try:
-            #print("Would edit {} (addition)".format(page.title()))
             i += 1
-            page.save(summary=TASK11_SUMMARY.format("Addition", _), minor=False)
+            if i >= 100:
+                print("Would edit {} (addition)".format(page.title()))
+            #page.save(summary=TASK11_SUMMARY.format("Addition", _), minor=False)
         except (pywikibot.exceptions.EditConflictError, pywikibot.exceptions.LockedPageError, pywikibot.exceptions.OtherPageSaveError):
             log_error(EDIT_FAIL_SUMMARY.format(page.title()), 11, soft=True)
-    if i >= 100:
-        break
+    #if i >= 100:
+    #    break
 
 print("Edits: "+str(i))
