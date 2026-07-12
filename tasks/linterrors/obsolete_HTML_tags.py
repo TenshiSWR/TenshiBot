958b59d1a5e14a31ab8b46f66db54ebdba63e101
Tenshi Hinanawi <203447253+TenshiSWR@users.noreply.github.com>
2026-07-12 12:01:34 +0100

    Fix missing ; cases

--- tasks/linterrors/obsolete_HTML_tags.py
+++ tasks/linterrors/obsolete_HTML_tags.py
@@ -45,7 +45,10 @@ def fix_obsolete_HTML_tags(page: str, text: str) -> str:
                 elif regex.search(r'size="?[^\s"]+"?', new_params, flags=regex.I):
                     new_params = regex.sub(r'size="?([^\s"]+)"?', r"font-size: \1", new_params, flags=regex.I)
                 new_params = regex.sub(r'face="([A-z ,.-]+)"', r"font-family: \1;", new_params, flags=regex.I)
-                new_params = regex.sub(r'style="(.+)"', r"\1", new_params, flags=regex.I)
+                if regex.search(r'style="[^"]+;"', new_params, flags=regex.I):
+                    new_params = regex.sub(r'style="([^"]+)"', r"\1", new_params, flags=regex.I)
+                else:
+                    new_params = regex.sub(r'style="([^"]+)"', r"\1;", new_params, flags=regex.I)
                 text = regex.sub(r'<font {}>'.format(regex.escape(params)), r'<span style="{}">'.format(new_params), text, flags=regex.I)
     text = regex.sub(r"(?<!(?:<(nowiki|syntaxhighlight)>(?!(?:(?:(?!(?:<\1>)).)*?)<\/\1>)|<\1>(?:(?:(?!(?:<\/?\1>|<\/?strike>)).)*?)<\/?strike>(?:(?:(?!(?:<\/?\1>)).)*?)<\/nowiki>))(<\/?)(?:[Ss]trike)>(?!(?!.*?<nowiki>).*?<\/nowiki>)", r"\2s>", text, flags=regex.I)
     for find, replace in regexes.items():
