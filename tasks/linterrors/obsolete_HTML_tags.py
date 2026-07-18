d9c66ced3ea74809c239718764a673fef824d091
Tenshi Hinanawi <203447253+TenshiSWR@users.noreply.github.com>
2026-07-18 12:34:02 +0100

    Spaces

--- tasks/linterrors/obsolete_HTML_tags.py
+++ tasks/linterrors/obsolete_HTML_tags.py
@@ -3,9 +3,9 @@ from tools.misc import LintfixModuleError, log_error
 
 regexes = {
     r"<center>((?:(?!(?:{\||\|}(?!\})|(?<!<center>)<\/center>|<\/center>(?=<\/center>)|<\/?gallery>|<center>[^<]*?(?!<\/center>[^<]*?<\/center>))).)*?)<\/center>": r'<div style="text-align: center;">\1</div>',
-    r'<center>([\n\s]*?\{\|[^<>|!]*?style="[^\"]+)(?<!; )(?<!(?:margin|margin-(?:bottom|left|right|up)):.*); *("[^\}]+(?:(?:(?!(?:\|\})).)*)\|\})[\n\s]*?<\/center>': r"\1; margin: auto;\2",
-    r'<center>([\n\s]*?\{\|(?![^\n]*?style="[^\"]+")[^\n]*)((?:(?:(?!(?:\|\})).)*?)\|\})[\n\s]*?<\/center>': r'\1 style="margin: auto;"\2',
-    r'<center>([\n\s]*?\{\|[^<>|!]*?style="(?:(?:(?!(?: "|")).)+?))(?<!;)(?<!(?:margin|margin-(?:bottom|left|right|up)):.*)("[^\}]+(?:(?:(?!(?:\|\})).)*?)\|\})[\n\s]*?<\/center>': r'\1; margin: auto;\2',
+    r'<center>([\n\s]*?\{\|[^<>|!]*?style *?= *?"[^\"]+)(?<!; )(?<!(?:margin|margin-(?:bottom|left|right|up)):.*); *("[^\}]+(?:(?:(?!(?:\|\})).)*)\|\})[\n\s]*?<\/center>': r"\1; margin: auto;\2",
+    r'<center>([\n\s]*?\{\|(?![^\n]*?style *?= *?"[^\"]+")[^\n]*)((?:(?:(?!(?:\|\})).)*?)\|\})[\n\s]*?<\/center>': r'\1 style="margin: auto;"\2',
+    r'<center>([\n\s]*?\{\|[^<>|!]*?style *?= *?"(?:(?:(?!(?: "|")).)+?))(?<!;)(?<!(?:margin|margin-(?:bottom|left|right|up)):.*)("[^\}]+(?:(?:(?!(?:\|\})).)*?)\|\})[\n\s]*?<\/center>': r'\1; margin: auto;\2',
     r'<center>([\n\s]*?<gallery(?:(?:(?!(?: ?[<>])).)*?)(?<!(?:class|perrow)="))(>.*?<\/gallery>[\n\s]*?)<\/center>': r'\1 class="center"\2',
     r'<center>([\n\s]*?<gallery[^<>]*(?<!(?:class|perrow)="))(?<! ) +(>.*?<\/gallery>[\n\s]*?)<\/center>': r'\1 class="center"\2'
 }
@@ -14,6 +14,7 @@ regexes = {
 def fix_obsolete_HTML_tags(page: str, text: str) -> str:
     text = regex.sub(r"(<font[^>]*?>[^<\n]*?)<font>(?!((?:(?!<\/font>(?![^<\n]*?<\/font>)).)*?)<\/font>)", r"\1</font\2>", text, flags=regex.DOTALL)  # Will pick up two opening tags even though they close twice
     text = regex.sub(r"<(\/)font[^>]*>", r"<\1span>", text, flags=regex.I)
+    #(<span[^>]*>)((?:(?!(?:<font(?: [^>\/]*)?>[^<]*(?!<\/font>)|(?<!<span[^>]*>[^<]*(?!<span[^>]*>))[^<]*)<\/span>).)*)<font>(?![^<]*<\/font>)
     if regex.search(r"<\/?font[^>]*>", text, flags=regex.I):
         tags = list(set(regex.findall(r"<\/?font[^>]*>", text, flags=regex.DOTALL|regex.I)))
         for font in tags:
@@ -22,14 +23,14 @@ def fix_obsolete_HTML_tags(page: str, text: str) -> str:
             if regex.search(r"<font [^>]+>", font, flags=regex.I):
                 params = regex.search(r"<font ([^>]+)>", font, flags=regex.I).group(1)
                 new_params = params
-                if regex.search(r'color="?([^\s"]+)"?', new_params, flags=regex.I):
-                    value = regex.search(r'color="?([^\s"]+)"?', new_params, flags=regex.I).group(1)
+                if regex.search(r'color *?= *?"?([^\s"]+)"?', new_params, flags=regex.I):
+                    value = regex.search(r'color *?= *?"?([^\s"]+)"?', new_params, flags=regex.I).group(1)
                     if regex.search(r"[0-9A-Fa-f]{6}", value):
-                        new_params = regex.sub(r'color="?([^\s"]+)"?', r"color: #{};".format(regex.search(r"#?([0-9A-Fa-f]{3,6})", value).group(1)), new_params, flags=regex.I)
+                        new_params = regex.sub(r'color *?= *?"?([^\s"]+)"?', r"color: #{};".format(regex.search(r"#?([0-9A-Fa-f]{3,6})", value).group(1)), new_params, flags=regex.I)
                     else:
-                        new_params = regex.sub(r'color="?([^\s"]+)"?', r"color: \1;", new_params, flags=regex.I)
-                if regex.search(r'size="?([+\-\d]\d*)(?:px)?"?', new_params, flags=regex.I):
-                    size = regex.search(r'size= *"? *([+\-\d]\d*)(?:px)?"?', new_params, flags=regex.I).group(1)
+                        new_params = regex.sub(r'color *?= *?"?([^\s"]+)"?', r"color: \1;", new_params, flags=regex.I)
+                if regex.search(r'size *?= *?"?([+\-\d]\d*)(?:px)?"?', new_params, flags=regex.I):
+                    size = regex.search(r'size *?= *?"? *([+\-\d]\d*)(?:px)?"?', new_params, flags=regex.I).group(1)
                     number = int(regex.search(r"\d+", size).group())
                     if "+" in size[0] or "-" in size[0]:
                         if "+" in size[0]:
@@ -42,13 +43,13 @@ def fix_obsolete_HTML_tags(page: str, text: str) -> str:
                         number = 1
                     new_size = ["x-small", "small", "medium", "large", "x-large", "xx-large", "xxx-large"][number-1]
                     new_params = regex.sub(r'size="?(.\d*)(?:px)?"?', r"font-size: {};".format(new_size), new_params, flags=regex.I)
-                elif regex.search(r'size="?[^\s"]+"?', new_params, flags=regex.I):
-                    new_params = regex.sub(r'size="?([^\s"]+)"?', r"font-size: \1", new_params, flags=regex.I)
-                new_params = regex.sub(r'face="([A-z ,.-]+)"', r"font-family: \1;", new_params, flags=regex.I)
-                if regex.search(r'style="[^"]+;"', new_params, flags=regex.I):
-                    new_params = regex.sub(r'style="([^"]+)"', r"\1", new_params, flags=regex.I)
+                elif regex.search(r'size *?= *?"?[^\s"]+"?', new_params, flags=regex.I):
+                    new_params = regex.sub(r'size *?= *?"?([^\s"]+)"?', r"font-size: \1", new_params, flags=regex.I)
+                new_params = regex.sub(r'face *?= *?"([A-z ,.-]+)"', r"font-family: \1;", new_params, flags=regex.I)
+                if regex.search(r'style *?= *?"[^"]+;"', new_params, flags=regex.I):
+                    new_params = regex.sub(r'style *?= *?"([^"]+)"', r"\1", new_params, flags=regex.I)
                 else:
-                    new_params = regex.sub(r'style="([^"]+)"', r"\1;", new_params, flags=regex.I)
+                    new_params = regex.sub(r'style *?= *?"([^"]+)"', r"\1;", new_params, flags=regex.I)
                 text = regex.sub(r'<font {}>'.format(regex.escape(params)), r'<span style="{}">'.format(new_params), text, flags=regex.I)
     text = regex.sub(r"(?<!(?:<(nowiki|syntaxhighlight)>(?!(?:(?:(?!(?:<\1>)).)*?)<\/\1>)|<\1>(?:(?:(?!(?:<\/?\1>|<\/?strike>)).)*?)<\/?strike>(?:(?:(?!(?:<\/?\1>)).)*?)<\/nowiki>))(<\/?)(?:[Ss]trike)>(?!(?!.*?<nowiki>).*?<\/nowiki>)", r"\2s>", text, flags=regex.I)
     for find, replace in regexes.items():
